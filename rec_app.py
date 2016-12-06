b7098ed915fd82238fcfcced292e113fb3481a75
Eduardo <ejcaropr@gmail.com>
2016-12-06 09:01:41 -0800

    added ajax

--- rec_app.py
+++ rec_app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from recommend import *
 DBNAME = 'lol.db'
 app = Flask(__name__)
 
-@app.route('/')
-def load_pred():
-    profile = [22,13]
+player_champs = get_player_champ_data(DBNAME)
 
-    player_champs = get_player_champ_data(DBNAME)
+@app.route('/', methods=['GET', 'POST'])
+def load_pred():
+#    if request.method == 'POST':
+#        profile = request.values.getlist('results')
+        #profile =[22,13]
+#        print(profile)
+#    else:
+#        profile = []
+    profile = request.form.getlist('profile[]')
+    if not profile:
+        profile = [22,13]
+    print(profile)
+
     weights = get_weights(player_champs, profile)
     preds = get_pred(player_champs, profile)
-
 
     return render_template('index.html', list1=profile, list2=preds.index)
